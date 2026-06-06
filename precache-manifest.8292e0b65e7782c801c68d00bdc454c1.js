d508b2ae69eebe1ab04dfe323cda49e0983e82ab
Matthew Tang <matthewtang@berkeley.edu>
2026-06-05 23:05:46 -0700

    Updates

---
+++ precache-manifest.8292e0b65e7782c801c68d00bdc454c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744e79114091d3f08fef0656915bfcfe",
+    "url": "/anime-family-feud/index.html"
+  },
+  {
+    "revision": "e5db3468ef1f14e1a04d",
+    "url": "/anime-family-feud/static/css/main.1e24408e.chunk.css"
+  },
+  {
+    "revision": "1cc0d1086a1ea4db8a1f",
+    "url": "/anime-family-feud/static/js/2.22558305.chunk.js"
+  },
+  {
+    "revision": "e5db3468ef1f14e1a04d",
+    "url": "/anime-family-feud/static/js/main.1ca81292.chunk.js"
+  },
+  {
+    "revision": "730034f1fa27905b4a79",
+    "url": "/anime-family-feud/static/js/runtime~main.9d19b7cf.js"
+  }
+]);
